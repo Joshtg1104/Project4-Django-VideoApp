c09c2a977642fc3f91b22cd6150ad880c41f4f90
Joshtg1104 <joshtg1104@gmail.com>
2019-05-29 16:05:59 -0500

    update

--- Project4Proj/Project4App/forms.py
+++ Project4Proj/Project4App/forms.py
@@ -1,6 +1,6 @@
 from django import forms
+
 from .models import Video
-from django.contrib.auth.models import User
 
 
 class VideoForm(forms.ModelForm):
--- Project4Proj/Project4App/views.py
+++ Project4Proj/Project4App/views.py
@@ -1,27 +1,39 @@
-from django.shortcuts import render
-from .models import Video
+from django.shortcuts import render, redirect
+
 from .forms import VideoForm
+from .models import Video
+
 
 # Create your views here.
 
 
-def index():
-    return print("Test")
+def index(request):
+    allVideos = Video.objects.all()
+    context = {
+        'allVideos': allVideos
+    }
+    return render(request, 'Project4App/index.html', context)
 
 
 def uploadvideo(request):
 
+    form = VideoForm(request.POST or None, request.FILES or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+
+            return redirect('index')
+
     lastvideo = Video.objects.last()
+    print(lastvideo)
 
     videofile = lastvideo.videofile
-
-    form = VideoForm(request.POST or None, request.FILES or None)
-    if form.is_valid():
-        form.save()
+    print(videofile)
 
     context = {
         'videofile': videofile,
         'form': form
     }
 
-    return render(request, 'index')
+    return render(request, 'Project4App/uploadVideo.html', context)
