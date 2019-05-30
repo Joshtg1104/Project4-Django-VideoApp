29167b95411d15ec71858300c8e43a3938c10690
Joshtg1104 <joshtg1104@gmail.com>
2019-05-30 14:23:14 -0500

    Yea Yeah

--- Project4Proj/Project4App/urls.py
+++ Project4Proj/Project4App/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('createAccount/', views.createAccount, name='createAccount'),
+    path('createAccount/', views.createaccount, name='createAccount'),
     path('uploadVideo/', views.uploadvideo, name='uploadVideo'),
     path('media/<path:path>/', serve, {'document_root': settings.MEDIA_ROOT, })
 ]
--- Project4Proj/Project4App/views.py
+++ Project4Proj/Project4App/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-
+from django.contrib.auth.models import User
 from .forms import VideoForm, AccountForm
 from .models import Video, AccountModel
 
@@ -13,6 +13,22 @@ def index(request):
         'allVideos': allVideos
     }
     return render(request, 'Project4App/index.html', context)
+
+
+def createaccount(request):
+    print("OK")
+    newaccountform = AccountForm(request.POST or None)
+    if request.method == "POST":
+        if newaccountform.is_valid():
+            newaccountform.save()
+
+            User.objects.create_user(request.POST["username"], request.POST["password"])
+            return redirect('index')
+    context = {
+        'errors': newaccountform.errors,
+        'newaccountform': newaccountform
+    }
+    return render(request, 'Project4App/createAccount.html', context)
 
 
 def uploadvideo(request):
