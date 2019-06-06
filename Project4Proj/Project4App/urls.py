62b92661c2149c4c7d286ef957306e8ea49d06b3
Joshtg1104 <joshtg1104@gmail.com>
2019-06-06 14:26:07 -0500

    Intro Index Complete... Sorta

--- Project4Proj/Project4App/urls.py
+++ Project4Proj/Project4App/urls.py
@@ -10,7 +10,5 @@ urlpatterns = [
     path('createAccount/', views.createaccount, name='createAccount'),
     path('uploadVideo/', views.uploadvideo, name='uploadVideo'),
     path('videoPage/<int:id>/', views.videopage, name='videoPage'),
-    # path('videoPage/<int:id>/commentSection', views.commentsection, name='commentSection'),
-    # path('commentSection/<int:id>/', views.commentsection, name='commentSection'),
     path('media/<path:path>/', serve, {'document_root': settings.MEDIA_ROOT, })
 ]
