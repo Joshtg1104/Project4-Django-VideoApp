05a310552c533fd79e0f3f8c92e1446fdf0e0721
student <joshtg1104@gmail.com>
2019-06-11 20:12:56 +0000

    Work baby Work

--- Project4Proj/Project4Proj/settings.py
+++ Project4Proj/Project4Proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u)_m__66-gkubyw=b+oe%b6zgg@5pkmjd3=21geyf%q=%4%^ct'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['134.209.166.44', 'app2.joshuagrahamcoding.com']
 
 
 # Application definition
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 #
